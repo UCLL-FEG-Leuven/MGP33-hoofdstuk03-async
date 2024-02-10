5264bf13fda4d167c40c4ad717ad74c137d6cfe0
David Urting <David.Urting@lv.vlaanderen.be>
2024-02-10 15:31:26 +0100

    callback klaar + issue met css opgelost

--- verkeerslicht.js
+++ verkeerslicht.js
@@ -1,10 +1,22 @@
 const checkbox = document.querySelector("input[type='checkbox']");
 const groen = document.getElementById("groen");
  
-checkbox.addEventListener('change', (e) => {
-  if (checkbox.checked) {
-    groen.setAttribute('class', 'active');
-  } else {
-    groen.setAttribute('class', '');
+checkbox.addEventListener('change', (e) => {  
+  if (checkbox.checked) {    
+    setInterval(() => {       
+      groen.setAttribute('class', 'active');
+      setTimeout(() => {
+        groen.setAttribute('class', '');
+        oranje.setAttribute('class', 'active');
+        setTimeout(() => {
+          oranje.setAttribute('class', '');
+          rood.setAttribute('class', 'active');
+          setTimeout(() => {
+            rood.setAttribute('class', '');
+            groen.setAttribute('class', 'active');
+          }, 3000);
+        }, 1500);
+      }, 3000);
+    }, 8000);
   }
 });
