b055df8f9c0e255437d584b205c4cbb9a54524aa
David Urting <David.Urting@lv.vlaanderen.be>
2024-02-10 18:27:10 +0100

    promise voorbeeld klaar

--- verkeerslicht.js
+++ verkeerslicht.js
@@ -1,22 +1,39 @@
+// const setTimeoutAsync = function (msec) {
+//   return new Promise(
+//     function(resolve, reject) {
+//       setTimeout(resolve, msec);
+//     });
+// } 
+ 	
+const setTimeoutAsync = msec =>
+ 	new Promise(resolve => setTimeout(resolve, msec));
+
 const checkbox = document.querySelector("input[type='checkbox']");
 const groen = document.getElementById("groen");
  
 checkbox.addEventListener('change', (e) => {  
   if (checkbox.checked) {    
-    setInterval(() => {       
+    setInterval(() => {
+      // 3 seconden groen
       groen.setAttribute('class', 'active');
-      setTimeout(() => {
-        groen.setAttribute('class', '');
-        oranje.setAttribute('class', 'active');
-        setTimeout(() => {
+      setTimeoutAsync(3000)
+        .then(() => {
+          // dan 1,5 seconden oranje
+          groen.setAttribute('class', '');
+          oranje.setAttribute('class', 'active');  
+          return setTimeoutAsync(1500); 
+        })
+        .then(() => {
+          // en vervolgens 3 seconden rood
           oranje.setAttribute('class', '');
           rood.setAttribute('class', 'active');
-          setTimeout(() => {
-            rood.setAttribute('class', '');
-            groen.setAttribute('class', 'active');
-          }, 3000);
-        }, 1500);
-      }, 3000);
+          return setTimeoutAsync(3000);
+        })
+        .then(() => {
+          // en weer groen
+          rood.setAttribute('class', '');
+          groen.setAttribute('class', 'active');
+        });
     }, 8000);
   }
 });
