7663619d57367a5437356dff511afe39171b34fc
David Urting <David.Urting@lv.vlaanderen.be>
2024-02-10 23:17:00 +0100

    async-await versie

--- verkeerslicht.js
+++ verkeerslicht.js
@@ -4,29 +4,26 @@ const setTimeoutAsync = msec =>
 const checkbox = document.querySelector("input[type='checkbox']");
 const groen = document.getElementById("groen");
  
-checkbox.addEventListener('change', (e) => {  
+checkbox.addEventListener('change', async (e) => {  
   if (checkbox.checked) {    
-    setInterval(() => {
+    setInterval(async () => {
       // 3 seconden groen
       groen.setAttribute('class', 'active');
-      setTimeoutAsync(3000)
-        .then(() => {
-          // dan 1,5 seconden oranje
-          groen.setAttribute('class', '');
-          oranje.setAttribute('class', 'active');  
-          return setTimeoutAsync(1500); 
-        })
-        .then(() => {
-          // en vervolgens 3 seconden rood
-          oranje.setAttribute('class', '');
-          rood.setAttribute('class', 'active');
-          return setTimeoutAsync(3000);
-        })
-        .then(() => {
-          // en weer groen
-          rood.setAttribute('class', '');
-          groen.setAttribute('class', 'active');
-        });
+      await setTimeoutAsync(3000);
+
+      // dan 1,5 seconden oranje
+      groen.setAttribute('class', '');
+      oranje.setAttribute('class', 'active');  
+      await setTimeoutAsync(1500); 
+
+      // en vervolgens 3 seconden rood
+      oranje.setAttribute('class', '');
+      rood.setAttribute('class', 'active');
+      await setTimeoutAsync(3000);
+
+      // en weer groen
+      rood.setAttribute('class', '');
+      groen.setAttribute('class', 'active');
     }, 8000);
   }
 });
